93db89a1090e6dbdd57bbc41db8c0852008c4970
Neil Poulin <neil.j.poulin@gmail.com>
2018-07-17 22:44:55 -0600

    testing gcloud

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 var airtable = require('./airtable')
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 
 const apiKey = 'fx0i3j3w2letmbq2g6rxlyk4'
@@ -9,19 +9,40 @@ const etsyDomain = 'https://openapi.etsy.com/v2'
 const listingsPath = '/listings/active'
 const listingIdPath = '/listings/:listingId'
 
-const csvWriter = createCsvWriter({
-    path: 'output/products.csv',
-    header: [
-        {id: 'title', title: 'Title'},
-        {id: 'tags', title: 'Tags'},
-        {id: 'materials', title: 'Materials'},
-        {id: 'image', title: 'Image'},
-    ]
-});
+// const csvWriter = createCsvWriter({
+//     path: 'output/products.csv',
+//     header: [
+//         {id: 'title', title: 'Title'},
+//         {id: 'tags', title: 'Tags'},
+//         {id: 'materials', title: 'Materials'},
+//         {id: 'image', title: 'Image'},
+//     ]
+// });
 
 const inputUrl = 'https://www.etsy.com/listing/568136358/light-grey-dog-bed-cover-custom?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=dog%20beds&ref=sc_gallery-1-2&plkey=c0d6159e3de06ca82b43102fed6bbba5e9f999ad:568136358&more_colors=1';
 
-function start(){
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!Object} req HTTP request context.
+ * @param {!Object} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+    let url = req.query.url;
+    if ( !url){
+        return res.status(400).send('You must provide a URL as a query parameter')
+    }
+
+    processUrl(url).then(response => {
+        return res.status(200).send('successfully processed url');
+    }).catch(error => {
+        return res.status(400).send(JSON.stringify(error))
+    });
+};
+
+
+exports.start = function(){
     // getListings();
     processUrl(inputUrl)
 }
@@ -30,15 +51,18 @@ function processUrl(url) {
     if (url.indexOf('www.etsy.com') !== -1){
         let listingId = url.split('www.etsy.com/')[1].split('/')[1]
         console.log('parsed listing ID ', listingId, 'from url')
-        getEtsyListing(listingId, url)
+        return getEtsyListing(listingId, url)
+    } else {
+        return Promise.reject({error: 'unsupported site.'})
     }
 }
 
 function getEtsyListing(listingId, url)
 {
-    axios.get(`${etsyDomain}${listingIdPath.replace(':listingId', listingId)}?api_key=${apiKey}&includes=MainImage`).then(({data}) =>  data).then(data => {
-        processListings(data.results, url);
-    })
+    return axios.get(`${etsyDomain}${listingIdPath.replace(':listingId', listingId)}?api_key=${apiKey}&includes=MainImage`)
+        .then(({data}) =>  data).then(data => {
+            return processListings(data.results, url);
+        })
 }
 
 function getListings()
@@ -76,7 +100,7 @@ function processListings(results, originalUrl)
     });
 
 
-    csvWriter.writeRecords(records);
+    // csvWriter.writeRecords(records);
 }
 
 function getImageUrlEtsy(listing)
@@ -84,5 +108,3 @@ function getImageUrlEtsy(listing)
     let fullImageUrl = listing.MainImage["url_fullxfull"]
     return fullImageUrl
 }
-
-start();
